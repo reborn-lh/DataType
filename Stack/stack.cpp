3e9c615a813185225277e40f7b0353783c82529e
s_chenlonghui <s_chenlonghui@wps.cn>
2026-07-09 10:08:58 +0800

    stack finished

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -2,22 +2,57 @@
 using namespace std;
 
 class Node{
-    private:
+    public:
     int val;
     Node* next;
-    public:
     Node(int v):val(v),next(nullptr){}
-    Node(int val, Node* n):val(val),next(n){}
+    Node(int v, Node* n):val(v),next(n){}
 };
 struct Stack{
-    private:
-    Node* head;
     public:
+    Node* head;
     Stack():head(nullptr){}
-    void push(int v){
-        Node* newNode = new Node(v);
-    }
+    bool isEmpty(Stack s);
+    void push(Stack& s,int v);
+    void pop(Stack& s);
+    int getTop(Stack& s);
 };
+bool isEmpty(Stack s){
+    return s.head==nullptr;
+}
+void push(Stack& s,int v){
+
+    Node* newNode = new Node(v);
+    newNode->next = s.head;
+    s.head = newNode;
+}
+void pop(Stack& s){
+    if(isEmpty(s)){
+        return;
+    }
+    Node* n=s.head;
+    s.head=s.head->next;
+    delete n;
+}
+int getTop(Stack& s){
+    return s.head->val;
+}
 int main() {
-    
-}   
+    Stack stacklist;
+    isEmpty(stacklist);
+    push(stacklist,1);
+    push(stacklist,2);
+    push(stacklist,3);
+    push(stacklist,4);
+    push(stacklist,5);
+    isEmpty(stacklist);
+    pop(stacklist);
+    getTop(stacklist);
+    pop(stacklist);
+    cout<<getTop(stacklist)<<endl;
+    while(!isEmpty(stacklist)){
+        cout<<getTop(stacklist)<<endl;
+        pop(stacklist);
+    }
+    return 0;
+}
